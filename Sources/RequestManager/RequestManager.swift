42fe9558e6cf2cb54e28a28f0cdd099e8181ae6c
Sukhaman Singh <sukhamandev@gmail.com>
2024-05-17 14:40:49 -0400

    initial commit

--- Sources/RequestManager/RequestManager.swift
+++ Sources/RequestManager/RequestManager.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public class RequestManager {
+    private init() {}
+    
+    public static func fetchData() {
+        print("Fetching data")
+    }
+}
