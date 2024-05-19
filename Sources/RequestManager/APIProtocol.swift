bc2a36fdec45250eaf22550c52e89cbd318c9138
Sukhaman Singh <sukhamandev@gmail.com>
2024-05-19 13:32:07 -0400

    Protocol made public to accessibility

--- Sources/RequestManager/APIProtocol.swift
+++ Sources/RequestManager/APIProtocol.swift
@@ -4,18 +4,18 @@
 
 import Foundation
 
-protocol APIProtocol {
+public protocol APIProtocol {
     static var baseUrl: URL {get}
 }
 
 extension RawRepresentable where RawValue == String, Self: APIProtocol {
-    var url: URL { Self.baseUrl.appendingPathComponent(rawValue)}
+   public var url: URL { Self.baseUrl.appendingPathComponent(rawValue)}
 }
 
-extension URL {
+public extension URL {
     func appending(_ queryItem: String, value: String?) -> URL {
-        guard var urlComponents = URLComponents(string: absoluteString) else { return absoluteURL }
-        var queryItems: [URLQueryItem] = urlComponents.queryItems ??  []
+        guard var urlComponents = URLComponents(string: self.absoluteString) else { return self.absoluteURL }
+        var queryItems: [URLQueryItem] = urlComponents.queryItems ?? []
         let queryItem = URLQueryItem(name: queryItem, value: value)
         queryItems.append(queryItem)
         urlComponents.queryItems = queryItems
