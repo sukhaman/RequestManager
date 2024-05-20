1ce7d10cf5a3d2785a14235df2f1cad07fc00167
Sukhaman Singh <sukhamandev@gmail.com>
2024-05-20 14:36:45 -0400

    RequestMethod added

--- Sources/RequestManager/APIProtocol.swift
+++ Sources/RequestManager/APIProtocol.swift
@@ -22,3 +22,11 @@ public extension URL {
         return urlComponents.url!
     }
 }
+
+public enum RequestMethod : String {
+    case get     = "GET"
+    case post    = "POST"
+    case put     = "PUT"
+    case patch   = "PATCH"
+    case delete  = "DELETE"
+}
